c291a7c4b6e65974eccb76011597f7659e88ccf7
Antonio Astorino <antonio.astorino.ing@gmail.com>
2020-09-07 22:17:13 +0200

    Started implementing tcp header parser

--- src/ip.rs
+++ src/ip.rs
@@ -1,17 +1,64 @@
 use std::net;
-pub struct Header {
+
+pub trait Printable {
+	fn print_me(&self);
+}
+
+pub struct CommonHeader {
 	pub flags: u16,
 	pub proto: u16,
 	pub version: u8,  // upper 4 bits only
 	pub ihl: u8,      // lower 4 bits only (internet header length)
 	pub tot_len: u16, // total length
-	pub ttl: u8,      // time to live
 	pub protocol: u8, // protocol
 	pub sender: net::Ipv4Addr,
 	pub receiver: net::Ipv4Addr,
+}
+
+pub struct IcmpHeader {
+	pub ttl: u8, // time to live
 	pub id: u16,
 	pub seq: u16,
 	pub inv_seq: u16,
+}
+
+pub struct TcpHeader {
+	pub sender_port: u16,
+	pub receiver_port: u16,
+}
+
+impl TcpHeader {
+	pub fn new(bytes: &[u8]) -> Self {
+		Self {
+			sender_port: u16::from_be_bytes([bytes[24], bytes[25]]),
+			receiver_port: u16::from_be_bytes([bytes[26], bytes[27]]),
+		}
+	}
+}
+
+impl Printable for TcpHeader {
+	fn print_me(&self) {
+		println!("{} -> {}", self.sender_port, self.receiver_port);
+	}
+}
+
+impl IcmpHeader {
+	pub fn new(bytes: &[u8]) -> Self {
+		Self {
+			ttl: bytes[12],
+			id: u16::from_be_bytes([bytes[28], bytes[29]]),
+			seq: u16::from_be_bytes([bytes[30], bytes[31]]),
+			inv_seq: u16::from_be_bytes([bytes[31], bytes[30]]),
+		}
+	}
+}
+impl Printable for IcmpHeader {
+	fn print_me(&self) {
+		println!(
+			"id={}, seq={}/{}, ttl={}",
+			self.id, self.seq, self.inv_seq, self.ttl
+		)
+	}
 }
 
 // converts an Assigned Internet Protocol Number into the corresponding name
@@ -23,7 +70,7 @@ pub fn aipn_to_str(code: u8) -> String {
 	}
 }
 
-impl Header {
+impl CommonHeader {
 	pub fn new(bytes: &[u8]) -> Self {
 		Self {
 			flags: u16::from_be_bytes([bytes[0], bytes[1]]),
@@ -31,13 +78,37 @@ impl Header {
 			version: bytes[4] >> 4,
 			ihl: bytes[4] & 0x0f,
 			tot_len: u16::from_be_bytes([bytes[6], bytes[7]]),
-			ttl: bytes[12],
 			protocol: bytes[13],
 			sender: net::Ipv4Addr::new(bytes[16], bytes[17], bytes[18], bytes[19]),
 			receiver: net::Ipv4Addr::new(bytes[20], bytes[21], bytes[22], bytes[23]),
-			id: u16::from_be_bytes([bytes[28], bytes[29]]),
-			seq: u16::from_be_bytes([bytes[30], bytes[31]]),
-			inv_seq: u16::from_be_bytes([bytes[31], bytes[30]]),
+		}
+	}
+
+	pub fn print_header(&self) {
+		println!(
+			"Flags: {:x?} Proto: {:x?} IP: v{} IHL: {} dw",
+			self.flags, self.proto, self.version, self.ihl
+		);
+		println!(
+			"{} -> {} {} {}",
+			self.sender,
+			self.receiver,
+			aipn_to_str(self.protocol),
+			self.tot_len,
+		);
+	}
+}
+
+pub fn print_header<T>(common_header: &CommonHeader, specific_header: &T)
+where
+	T: Printable,
+{
+	match common_header.protocol {
+		1 | 6 => {
+			specific_header.print_me();
+		}
+		_ => {
+			println!("unknown");
 		}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -12,43 +12,43 @@ fn exec(cmd: &str, args: &[&str]) {
     assert!(ecode.success(), "Failed to execte {}", cmd);
 }
 
+fn init_iface(iface_name: &str, ip_addr: &'static str) {
+            exec(
+                "ip",
+                &["addr", "add", ip_addr, "dev", iface_name],
+            );
+            exec("ip", &["link", "set", "up", "dev", iface_name]);
+}
+
 fn main() -> io::Result<()> {
     let iface = tun_tap_new::Iface::new("", tun_tap_new::Mode::Tun);
     match iface {
         Ok(iface) => {
             println!("Interface `{}` successfully created", iface.name());
             // Configure the device ‒ set IP address on it, bring it up.
-            println!("Setting IP address");
-            exec(
-                "ip",
-                &["addr", "add", "192.168.0.1/24", "dev", iface.name()],
-            );
-            println!("Link up");
-            exec("ip", &["link", "set", "up", "dev", iface.name()]);
+            init_iface(iface.name(), "192.168.0.1/24");
             loop {
                 let mut buffer = vec![0u8; 1504]; // MTU + 4 for the header
                 let nbytes = iface.recv(&mut buffer).unwrap();
-
-                let header: ip::Header = ip::Header::new(&buffer);
+                let header: ip::CommonHeader = ip::CommonHeader::new(&buffer);
                 if header.proto != 0x0800 {
                     continue;
                 }
-                println!("Received {} bytes: {:x?}", nbytes, &buffer[..nbytes]);
-                println!(
-                    "Flags: {:x?} Proto: {:x?} IP: v{} IHL: {} dw",
-                    header.flags, header.proto, header.version, header.ihl
-                );
-                println!(
-                    "{} -> {} {} {} id=0x{:x?}, seq={}/{}, ttl={}",
-                    header.sender,
-                    header.receiver,
-                    ip::aipn_to_str(header.protocol),
-                    header.tot_len,
-                    header.id,
-                    header.seq,
-                    header.inv_seq,
-                    header.ttl
-                );
+                header.print_header();
+                match header.protocol {
+                    1 => {
+                        let specific_header = ip::IcmpHeader::new(&buffer);
+                        ip::print_header(&header, &specific_header);
+                    }
+                    6 => {
+                        let specific_header = ip::TcpHeader::new(&buffer);
+                        ip::print_header(&header, &specific_header);
+                        println!("{:x?}", &buffer[..nbytes]);
+                    } 
+                    _ => {
+                        println!("Unknown protocol");
+                    }
+                }
             }
         }
         Err(e) => {
